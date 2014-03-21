c194d68eac376e05b67f31e82b96529838b1af43
marcioalves <marcioalves.ti@gmail.com>
2014-03-21 11:17:16 -0300

    Criando a Home/index e gem devise instalada

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'capybara'
 
 gem 'rspec-rails'
 
+gem 'devise'
+
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 MiniApp::Application.routes.draw do
+  devise_for :users
+  get "home/index"
   resources :tasks
 
   resources :task_lists
 
   resources :users
+
+  root :to => "home#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
