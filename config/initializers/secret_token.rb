4f8fd446c20b29fec293b45b5c85484a216a7793
marcioalves <marcioalves.ti@gmail.com>
2014-03-21 02:56:06 -0300

    Inicio, Twitter bootstrap e simple_form instalado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiniApp::Application.config.secret_key_base = 'b2d09599d07ed60f61306c3fb5a84f5dae75850873e94f566f9f3beeb595840a452873854a7b0001e0ca1b1422512893d719686aa9fafe8d36be42fea0d28f3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MiniApp::Application.config.session_store :cookie_store, key: '_mini_app_session'
